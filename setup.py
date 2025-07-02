3f344a0ab136ac114f914c34f10e55e257deebbb
petter-b <62076402+petter-b@users.noreply.github.com>
2025-07-02 17:09:47 +0000

    Trying to get CI workflow to pass.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class CustomDistribution(Distribution):
         sys.path.insert(0, str(src_path))
 
         try:
-            from preservationeval._version import version as file_version # noca
+            from preservationeval._version import version as file_version  # noca
         except ImportError:
             file_version = "unknown"
 
@@ -79,7 +79,7 @@ class CustomDistribution(Distribution):
         sys.path.insert(0, str(src_path))
 
         try:
-            from preservationeval._version import version # noca
+            from preservationeval._version import version  # noca
 
             return str(version)
         except ImportError:
@@ -100,7 +100,9 @@ class CustomBuildPy(build_py):
 
         try:
             if not self.dry_run:
-                from preservationeval.install.generate_tables import generate_tables # noca
+                from preservationeval.install.generate_tables import (
+                    generate_tables,  # noca
+                )
 
                 generate_tables()
         except Exception as e:
